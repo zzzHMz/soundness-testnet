bf62616239052acf32f71ae378fd9d919714c199
zzzHmz <89087200+zzzHMz@users.noreply.github.com>
2025-09-18 12:37:27 +0700

    Create packcage.js

---
+++ packcage.js
@@ -0,0 +1,50 @@
+{
+  "name": "tea-fctanba",
+  "version": "3.0.0",
+  "private": false,
+  "dependencies": {
+    "@thirdweb-dev/react": "^3.5.1",
+    "@thirdweb-dev/sdk": "^3.5.1",
+    "ethers": "^5.6.6",
+    "hieuwb-tea-quest": "^3.0.0",
+    "react": "^18.0.2",
+    "react-dom": "^18.0.2",
+    "react-scripts": "5.0.0"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/zzzHMz/fctanba-tea.git"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+  },
+  "eslintConfig": {
+    "extends": [
+      "react-app"
+    ]
+  },
+  "browserslist": {
+    "production": [
+      ">0.2%",
+      "not dead",
+      "not op_mini all"
+    ],
+    "development": [
+      "last 1 chrome version",
+      "last 1 firefox version",
+      "last 1 safari version"
+    ]
+  },
+  "description": "https://wibudao.vercel.app/\r ### **Welcome 👋**\r To get started with this project, clone this repo and follow these:",
+  "main": "index.js",
+  "keywords": [
+    "npm",
+    "dao",
+    "nft"
+  ],
+  "author": "hieuwb",
+  "license": "ISC"
+}
